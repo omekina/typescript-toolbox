3d52fb79a7f41963f22d0a6dd5905f2e78418b84
Ondřej Mekina <mekina@duck.com>
2024-03-05 23:03:46 +0100

    Further simplify sleep function

--- src/typescript-toolbox/sync/sleep.ts
+++ src/typescript-toolbox/sync/sleep.ts
@@ -3,6 +3,5 @@
  * @param timeout in milliseconds
  */
 export default async function sleep(timeout: number): Promise<void> {
-    const promise = new Promise<void>(resolve => setTimeout(resolve, timeout));
-    await promise;
+    return new Promise<void>(resolve => setTimeout(resolve, timeout));
 }
